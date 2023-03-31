1508bc6cd3543b87b8158398a0c24f022e9be127
Aliaksei Burau <aburau@google.com>
2023-03-31 15:00:15 +0000

    Remove repo clean-up logic

--- agentendpoint/config_task.go
+++ agentendpoint/config_task.go
@@ -344,8 +344,9 @@ func (c *configTask) cleanupRepos(ctx context.Context) {
 }
 
 func (c *configTask) cleanup(ctx context.Context) {
-	// Purge repositories that are no longer managed.
-	c.cleanupRepos(ctx)
+	// Skip repo clean-up, as there is no clear way how to find full list of repos,
+	// Repos might be distributed across many many tasks and there is no source of truth.
+	//c.cleanupRepos(ctx)
 
 	// Cleanup any policy specific resources.
 	for _, osPolicy := range c.Task.GetOsPolicies() {
